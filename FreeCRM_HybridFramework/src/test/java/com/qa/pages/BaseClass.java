bab2626e6007391999e2b7d4005065f3b1af711f
Harvinder <mrharrysingh92@gmail.com>
2022-07-24 23:26:36 +0530

    Updated base class file

--- FreeCRM_HybridFramework/src/test/java/com/qa/pages/BaseClass.java
+++ FreeCRM_HybridFramework/src/test/java/com/qa/pages/BaseClass.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
@@ -42,10 +43,12 @@ public class BaseClass {
 		Reporter.log("Setting Done - Test can be started", true);
 	}
 	
+	@Parameters({"browser","urlToBeTested"})	//Passing parameters from the xmlFile
 	@BeforeClass
-	public void setUp() {
+	public void setUp(String browser, String url) {
 		Reporter.log("Trying to start browser and Getting application ready",true);
-		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingUrl());
+//		driver = BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingUrl());
+		driver = BrowserFactory.startApplication(driver, browser, url);
 		Reporter.log("Browser and Application is up and running",true);
 	}
 	
